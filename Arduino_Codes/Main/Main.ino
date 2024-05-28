fdda22066676d553a04bd00c3fb6f1ba3b5e9499
Lawrence Roble <lawrence7roble@gmail.com>
2024-05-28 17:07:33 +0800

    Update.

--- Arduino_Codes/Main/Main.ino
+++ Arduino_Codes/Main/Main.ino
@@ -1,18 +1,118 @@
-#include <RH_ASK.h>
-#include <SPI.h>
+#include <SoftwareSerial.h>
 
-RH_ASK RFsender;
+SoftwareSeraial BT;
+const byte EN1 = 2;
+const byte EN2 = 3;
+const byte EN3 = 4;
+const byte EN4 = 5;
 
 void setup() {
   Serial.begin(9600);
-  if (!RFsender.init()) {
-    Serial.println("Init Failed");
-  }
+  BT.begin(115200);
+
+  pinMode(EN1, OUTPUT);
+  pinMode(EN2, OUTPUT);
+  pinMode(EN3, OUTPUT);
+  pinMode(EN4, OUTPUT);
 }
 
 void loop() {
-  const char *msg = "Hello World!";
-  RFsender.send((uint8_t *)msg, strlen(msg));
-  RFsender.waitPacketSent();
-  delay(1000);
+  String receiveString = "";
+  bool BTreading = false;
+  int stringLength = 0;
+
+  while (BT.available > 0) {
+    BTreading = true;
+    char BTdata = BT.read();
+    if (BTdata != '\n' and BTdata != '\r')
+      receiveString += BTdata; 
+  }
+
+  if (BTreading) {
+    receiveString.trim();
+    stringLength = receiveString.length();
+  }
+
+  int count = 0;
+  while (count < stringLength) {
+    unsigned long currentMillis = millis();
+    /*
+      * Bug:
+      *   1. Indentify its current position.
+      *   2. Adjust the current position, depending on the next move.
+      *   3. Make a move with millis.
+      *   4. If there an interruption, like obstacles, during on its way, send a signal ('C') to Camera to create a new path and break this while-loop.
+      *   5. Repeat.
+      *
+    */
+    switch(receiveString[count]) {
+      case 'T':
+        MoveTop();
+        break;
+
+      case 'R':
+        MoveRight();
+        break;
+
+      case 'L':
+        MoveLeft();
+        break;
+
+      case 'B':
+        MoveBottom();
+        break;
+
+      default:
+        Stop();
+        break;
+    }
+  }
+}
+
+void MoveForward() {
+  digitalRead(EN1, 1);
+  digitalRead(EN2, 0);
+
+  digitalRead(EN3, 0);
+  digitalRead(EN4, 1);
+}
+
+void MoveTop() {
+  digitalRead(EN1, 1);
+  digitalRead(EN2, 0);
+
+  digitalRead(EN3, 0);
+  digitalRead(EN4, 1);
+}
+
+void MoveRight() {
+  digitalRead(EN1, 1);
+  digitalRead(EN2, 0);
+
+  digitalRead(EN3, 1);
+  digitalRead(EN4, 0);
+}
+
+void MoveLeft() {
+  digitalRead(EN1, 0);
+  digitalRead(EN2, 1);
+
+  digitalRead(EN3, 0);
+  digitalRead(EN4, 1);
+}
+
+void MoveBottom() {
+  digitalRead(EN1, 0);
+  digitalRead(EN2, 1);
+
+  digitalRead(EN3, 1);
+  digitalRead(EN4, 0);
+}
+
+void Stop() {
+  digitalRead(EN1, 0);
+  digitalRead(EN2, 0);
+
+  digitalRead(EN3, 0);
+  digitalRead(EN4, 0);
 }
